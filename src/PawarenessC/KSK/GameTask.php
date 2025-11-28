a04a6802c9b77b894282ec72017ab0b615c2e114
awa/Hanyu <35727789+PawarenessC@users.noreply.github.com>
2025-11-28 16:47:08 +0900

    Update GameTask.php

--- src/PawarenessC/KSK/GameTask.php
+++ src/PawarenessC/KSK/GameTask.php
@@ -2,29 +2,30 @@
 
 namespace PawarenessC\KSK;
 
-use PawarenessC\KSK\KSK;
+use pocketmine\Server;
 use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat;
+use function pocketmine\server;
 
-class GameTask extends Task{
-    public KSK $main;
-    public function __construct(KSK $main){
+class GameTask extends Task {
+    public $main;
+
+    public function __construct(KSK $main) {
+        $this->main = $main;
     }
+
     /*
      * 待機時間 80秒
      * ゲーム時間 420秒*/
-    public function onRun() : void{
+    public function onRun(): void {
         $main = $this->main;
         $game_running = $main->game_running;
-        if(!$game_running){
-            $main->time_start--;
-            //何秒後にスタートと表示
-            //残り30秒ぐらいでマップ抽選
-            //残り10秒ぐらいでテレポート スプラみたいにハンターだけを見下ろす
-        }
-        if($game_running){
+        if ($game_running) {
             $main->time_left--;
-            //60秒ごとに生きてる人の名前公表しますか
+            $blinkColor = ($main->time_left % 2 === 0) ? "§l" : "§7§o";
+            $minutes = floor(($main->time_left / 60) % 60);
+            $seconds = str_pad($main->time_left % 60, 2, "0", STR_PAD_LEFT);
+            $main->sendPopup("§f残り時間:§l§f{$blinkColor}{$minutes}§r§b:§r{$blinkColor}{$seconds}§r§e§r\n     §l§a鬼" . $main->remaining_hunters . " §cvs §b子ども " . $main->remaining_runners . "\n\n");
         }
     }
-
 }
